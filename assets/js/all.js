f3bd2753e00b2f71940f2cd7646e817702daa42b
amy <amy>
2022-08-10 17:37:15 +0800

    Update 2022-08-10T09:37:12.662Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,27 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  // console.log('Hello Bootstrap5');
+  // ckeditor
+  ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
+    console.log(editor);
+  })["catch"](function (error) {
+    console.error(error);
+  }); // 左側選單 
+
+  $(".nav li").click(function (e) {
+    e.preventDefault(); // console.log($(this).siblings())
+
+    $(this).children('a').addClass('active');
+    $(this).siblings().children('a').removeClass('active');
+  });
+  $(".reply-btn").click(function (e) {
+    e.preventDefault();
+    $(".reply-footer").toggleClass("d-none");
+  });
+  $(".cancel-btn").click(function (e) {
+    e.preventDefault();
+    $(".reply-footer").toggleClass("d-none");
+  });
 });
 //# sourceMappingURL=all.js.map
