20f30d828c4abe1577990089f1183620dd5076df
amy <amy>
2022-08-10 17:36:40 +0800

    Assignment 完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,30 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  // console.log('Hello Bootstrap5');
+  // ckeditor
+  ClassicEditor
+    .create( document.querySelector( '#editor' ) )
+    .then( editor => {
+    console.log( editor );
+  } )
+    .catch( error => {
+    console.error( error );
+  } );
+
+  // 左側選單 
+  $(".nav li").click(function(e) {
+    e.preventDefault();
+    // console.log($(this).siblings())
+    $(this).children('a').addClass('active');
+    $(this).siblings().children('a').removeClass('active');
+  });
+
+  $(".reply-btn").click(function(e) {
+    e.preventDefault();
+    $(".reply-footer").toggleClass("d-none");
+  });
+
+  $(".cancel-btn").click(function(e) {
+    e.preventDefault();
+    $(".reply-footer").toggleClass("d-none");
+  });
 });
