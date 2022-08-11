5cf84cc8aefd5e410fa40bd1bc9a18624f2e0a25
amy <amy>
2022-08-11 17:22:10 +0800

    Update 2022-08-11T09:22:07.825Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -10,8 +10,8 @@ $(function () {
   }); // 左側選單 
 
   $(".nav li").click(function (e) {
-    e.preventDefault(); // console.log($(this).siblings())
-
+    // e.preventDefault();
+    // console.log($(this).siblings())
     $(this).children('a').addClass('active');
     $(this).siblings().children('a').removeClass('active');
   });
@@ -22,6 +22,13 @@ $(function () {
   $(".cancel-btn").click(function (e) {
     e.preventDefault();
     $(".reply-footer").toggleClass("d-none");
+  });
+  $(".verified-btn").click(function (e) {
+    e.preventDefault();
+    $(".verified-expand-more").toggleClass("d-inline-block");
+    $(".verified-expand-less").toggleClass("d-inline-block");
+    $(".verified-expand-more").toggleClass("d-none");
+    $(".verified-expand-less").toggleClass("d-none");
   });
 });
 //# sourceMappingURL=all.js.map
